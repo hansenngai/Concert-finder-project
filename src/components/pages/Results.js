41c0ed9867b8ad7d4bca312dbf64ddadf537000f
Hansen Ngai <ngaih1@mcmaster.ca>
2019-10-03 14:41:41 -0400

    static picture
    screenshot add

--- src/components/pages/Results.js
+++ src/components/pages/Results.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Table from './../items/Table'
+import Samplepicture from './../../images/samplepicture.png'
 
 export default function () {
     return (
         <div>
             <Table />
+
+            <img src={Samplepicture}></img>
         </div>
     )
 }
