920e7bd0f91548819b7cff290ac83814c51d6bbf
Hansen Ngai <ngaih1@mcmaster.ca>
2019-10-03 17:36:49 -0400

    header test
    header test

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ import Searchbar from '../items/Searchbar'
 function Header() {
     return (
         <div>
-            <h1>Encore</h1>
+            <h1>Encore site</h1>
             <Link to="/">Home</Link> | <Link to="/login">Login</Link> | <Link to="/register">Register</Link> | <Link to="/submission">Submit an entry</Link>  
         </div>
     )
