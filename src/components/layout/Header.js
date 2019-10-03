ea9a6a6b5b73a24db504e30d7bfeef4d97109806
Tyler Simon Philips <t.philips90@gmail.com>
2019-10-03 15:36:01 -0400

    submit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import logo from './../../images/logo.png'
 function Header() {
     return (
         <div>
-            <h1>welcome</h1>
+        	
+        	<img src={logo} width="200" height="60"></img>
             <Link to="/">Home</Link> | <Link to="/login">Login</Link> | <Link to="/submission">Submit an entry</Link>
         </div>
     )
+
+
+
+
 }
 
 export default Header;
