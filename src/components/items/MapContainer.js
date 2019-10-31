c14f0bf2f02b9512dba17e638caa86ac8ca85f06
Hansen Ngai <ngaih1@mcmaster.ca>
2019-10-31 12:01:17 -0400

    marker in maps

--- src/components/items/MapContainer.js
+++ src/components/items/MapContainer.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 //trying to add a link inside the infowindow
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, withRouter } from 'react-router-dom';
 import ObjectPage from './../pages/ObjectPage';
 
 import CurrentLocation from './Map';
+import InfoWindowEx from './InfoWindowEx';
 
 export class MapContainer extends Component {
   state = {
@@ -40,7 +41,18 @@ export class MapContainer extends Component {
         {/* hard coded example location, ex. pub on emerson*/}
         <Marker position={{ lat: 43.253231, lng: -79.921517}} onClick={this.onMarkerClick} name={'PUB'} />
 
-        <InfoWindow
+        <InfoWindowEx
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+         <Router>
+        <Link to = '/objectpage' >
+        <button type="button" onClick={this.showDetails}>
+            Show details      
+        </button>
+        </Link>
+        </Router>
+        </InfoWindowEx>
+        {/* <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
           onClose={this.onClose}
@@ -49,13 +61,16 @@ export class MapContainer extends Component {
           <div>
             <h4>{this.state.selectedPlace.name}</h4>
           </div>
-        </InfoWindow>
+        </InfoWindow> */}
       </CurrentLocation>
 
 
     );
   }
 }
+
+//export default withRouter(MapContainer);
+//const MapContainer2 = withRouter(MapContainer);
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCjNntlsCbXB1MPAjkWF_19ZVO2gfgj8m8'
